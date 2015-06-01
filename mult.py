64a68eb232c462407880d1eacd76d4a25c92312b
Joseph Tagg <joseph@jtagg.net>
2015-06-01 20:38:39 +0200

    changed to english and added global constant

--- mult.py
+++ mult.py
@@ -5,11 +5,12 @@ import time
 import random
 
 NUM_MULT = 10
+MAX_MULT = 10
 
 def main(argv):
     table_selected = False
     while not table_selected:
-        print('Quelle table veux-tu pratiquer? (2-10) >> ', end='', flush=True)
+        print('Which times table do you want to practice (1-{}, empty for all)? >> '.format(MAX_MULT), end='', flush=True)
         table = sys.stdin.readline()
         if table == '\n':
             table = 0
@@ -20,15 +21,15 @@ def main(argv):
             except ValueError:
                 pass
             else:
-                if table >= 2 and table <= 10:
+                if table > 0 and table <= MAX_MULT:
                     table_selected = True
 
     correct_answers = 0
     start_time = time.time()
     for i in range(0, NUM_MULT):
-        multiplicand = random.randint(2, 10)
+        multiplicand = random.randint(2, MAX_MULT)
         if table == 0:
-            multiplier = random.randint(2, 10)
+            multiplier = random.randint(2, MAX_MULT)
         else:
             multiplier = table
         print('{: >-2d} x {} = '.format(multiplicand, multiplier), end='', flush=True)
@@ -44,7 +45,7 @@ def main(argv):
                 print("Faux\n")
     end_time = time.time()
     total_time = end_time - start_time
-    print("Tu as pris {:.2f} secondes et as une note de {}/{}.".format(total_time, correct_answers, NUM_MULT))
+    print("You needed {:.2f} seconds with a grade of {}/{}.".format(total_time, correct_answers, NUM_MULT))
 
 if __name__ == "__main__":
     main(sys.argv[1:])
